07486e77ad4fd0eb5d262a2d591604cc3c52c482
Elizaveta Pavlovskaya <lpavlovskayalp@mail.ru>
2025-03-04 17:21:04 +0500

    add 5

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
-const { log } = require('console');
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
 
 const files = getFiles();
-const listTODO = [];
-const listIMP = [];
-const dict = [];
-
-getListTODO(files);
-console.log(listTODO)
-console.log(listIMP);
-function getListTODO(files) {
-    for (const file of files) {
-        const strings = file.split('\n'); // Разделяем строки по \n
-        for (const str of strings) {
-            if (str.includes('// TODO ')) {
-                const comment = str.substring(str.indexOf('// TODO ') + 8).trim();
-                listTODO.push(comment);
-                const arr = comment.split(';');
-                if (arr.length === 3) {
-                    const name = arr[0].toLowerCase();
-                    const date = arr[1];
-                    const text = arr[2];
-                    dict.push({name, date, text})
-                }
-                if (comment.includes("!")) {
-                    listIMP.push(comment);
-                }
-            }
-        }
-    }
-}
-
+const todos = getTodos(files);
 
 console.log('Please, write your command!');
 readLine(processCommand);
+
+function getTodos(files) {
+    const todos = [];
+    for (const file of files) {
+        const lines = file.split('\n');
+        for (const line of lines) {
+            if (line.includes('// TODO ')) {
+                const todo = line.substring(line.indexOf('// TODO ') + 8).trim();
+                todos.push(todo);
+            }
+        }
+    }
+    return todos;
+}
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
@@ -42,30 +27,43 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    //console.log(files);
-    switch (command) {
+    const [action, arg] = command.split(' ');
+
+    switch (action) {
         case 'exit':
             process.exit(0);
             break;
+
         case 'show':
-            console.log(listTODO);
-            console.log(listTODO.length);
+            console.log(todos);
             break;
-        case 'important':
-            console.log(listIMP);
-        default:
-            if (command.split(' ')[0] === 'user') {
-                const name2 = command.split(' ')[1];
-                for (const element of dict) {
-                    if (element.name === name2) {
-                        console.log(element);
-                    }
-                    
+
+        case 'sort':
+            if (arg === 'importance') {
+                const sorted = todos.slice().sort((a, b) => (b.match(/!/g) || []).length - (a.match(/!/g) || []).length);
+                console.log(sorted);
+            } else if (arg === 'user') {
+                const grouped = {};
+                for (const todo of todos) {
+                    const [user] = todo.split(';');
+                    const name = user.trim().toLowerCase() || 'unnamed';
+                    if (!grouped[name]) grouped[name] = [];
+                    grouped[name].push(todo);
                 }
+                for (const user in grouped) {
+                    console.log(`User: ${user}`);
+                    grouped[user].forEach(todo => console.log(`  - ${todo}`));
+                }
+            } else if (arg === 'date') {
+                const withDate = todos.filter(todo => todo.split(';').length === 3);
+                const withoutDate = todos.filter(todo => todo.split(';').length !== 3);
+                withDate.sort((a, b) => new Date(b.split(';')[1].trim()) - new Date(a.split(';')[1].trim()));
+                console.log([...withDate, ...withoutDate].join('\n'));
             }
+            break;
+
+        default:
             console.log('wrong command');
             break;
     }
 }
-
-// TODO you can do it!
