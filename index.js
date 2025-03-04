8c8a10c67949320c8c82fbd2b6c7d032ca0bb9fd
Elizaveta Pavlovskaya <lpavlovskayalp@mail.ru>
2025-03-04 17:42:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,37 @@ const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
 const { readLine } = require('./console');
 
 const files = getFiles();
-const todos = getTodos(files);
+const listTODO = [];
+const listIMP = [];
+const dict = [];
+
+getListTODO(files);
 
 console.log('Please, write your command!');
 readLine(processCommand);
 
-function getTodos(files) {
-    const todos = [];
+function getListTODO(files) {
     for (const file of files) {
-        const lines = file.split('\n');
-        for (const line of lines) {
-            if (line.includes('// TODO ')) {
-                const todo = line.substring(line.indexOf('// TODO ') + 8).trim();
-                todos.push(todo);
+        const strings = file.split('\n');
+        for (const str of strings) {
+            if (str.includes('// TODO ')) {
+                const comment = str.substring(str.indexOf('// TODO ') + 8).trim();
+                listTODO.push(comment);
+
+                const arr = comment.split(';');
+                if (arr.length === 3) {
+                    const name = arr[0].toLowerCase();
+                    const date = arr[1];
+                    const text = arr[2];
+                    dict.push({ name, date, text });
+                }
+
+                if (comment.includes('!')) {
+                    listIMP.push(comment);
+                }
             }
         }
     }
-    return todos;
 }
 
 function getFiles() {
@@ -35,30 +49,30 @@ function processCommand(command) {
             break;
 
         case 'show':
-            console.log(todos);
+            console.log(listTODO);
+            console.log(`Total TODOs: ${listTODO.length}`);
+            break;
+
+        case 'important':
+            console.log(listIMP);
+            break;
+
+        case 'user':
+            const name2 = command.split(' ')[1];
+            for (const element of dict) {
+                if (element.name === name2) {
+                    console.log(element);
+                }
+            }
             break;
 
         case 'sort':
             if (arg === 'importance') {
-                const sorted = todos.slice().sort((a, b) => (b.match(/!/g) || []).length - (a.match(/!/g) || []).length);
-                console.log(sorted);
+                sortImportance();
             } else if (arg === 'user') {
-                const grouped = {};
-                for (const todo of todos) {
-                    const [user] = todo.split(';');
-                    const name = user.trim().toLowerCase() || 'unnamed';
-                    if (!grouped[name]) grouped[name] = [];
-                    grouped[name].push(todo);
-                }
-                for (const user in grouped) {
-                    console.log(`User: ${user}`);
-                    grouped[user].forEach(todo => console.log(`  - ${todo}`));
-                }
+                sortUser();
             } else if (arg === 'date') {
-                const withDate = todos.filter(todo => todo.split(';').length === 3);
-                const withoutDate = todos.filter(todo => todo.split(';').length !== 3);
-                withDate.sort((a, b) => new Date(b.split(';')[1].trim()) - new Date(a.split(';')[1].trim()));
-                console.log([...withDate, ...withoutDate].join('\n'));
+                sortDate();
             }
             break;
 
@@ -66,4 +80,47 @@ function processCommand(command) {
             console.log('wrong command');
             break;
     }
+}
+
+function sortImportance() {
+    const sorted = listTODO.slice().sort((a, b) => {
+        const countA = (a.match(/!/g) || []).length;
+        const countB = (b.match(/!/g) || []).length;
+        return countB - countA;
+    });
+    console.log(sorted);
+}
+
+function sortUser() {
+    const grouped = {};
+    for (const todo of listTODO) {
+        const [user] = todo.split(';');
+        const name = user.trim().toLowerCase() || 'unnamed';
+        if (!grouped[name]) grouped[name] = [];
+        grouped[name].push(todo);
+    }
+
+    for (const user in grouped) {
+        console.log(`User: ${user}`);
+        grouped[user].forEach(todo => console.log(`  - ${todo}`));
+    }
+}
+
+function sortDate() {
+    const withDate = [];
+    const noDate = [];
+
+    for (const todo of listTODO) {
+        const arr = todo.split(';');
+        if (arr.length === 3 && arr[1].trim()) {
+            withDate.push({ todo, date: new Date(arr[1].trim()) });
+        } else {
+            noDate.push(todo);
+        }
+    }
+
+    withDate.sort((a, b) => b.date - a.date);
+
+    withDate.forEach(item => console.log(item.todo));
+    noDate.forEach(todo => console.log(todo));
 }
